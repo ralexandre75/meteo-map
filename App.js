57ca329694466c68af696c998540fc2ef5e513b0
ralexandre75 <rene.alexandre@grimoire-design.fr>
2019-04-23 15:01:02 +0200

    show a map google map

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { MapView } from "expo";
+
+const DEFAULT_COORD = {
+  lat: 48.859268,
+  lng: 2.347060
+}
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <MapView
+          style={{flex : 1}}
+          region={{ latitude: DEFAULT_COORD.lat, longitude: DEFAULT_COORD.lng, latitudeDelta:0.2000, longitudeDelta: 0.1000}}
+          scrollEnabled={false}
+          liteMode={true}
+        >
+
+        </MapView>
       </View>
     );
   }
@@ -14,8 +27,5 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
